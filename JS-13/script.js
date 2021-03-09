81f1a4686d953e6bc56c0cf0f5a7124f7178f3ec
JoJovanna <75172430+JoJovanna@users.noreply.github.com>
2021-03-09 22:28:14 +0300

    Add files via upload

---
+++ JS-13/script.js
@@ -0,0 +1,7 @@
+//Напиши код, в котором ты спрашиваешь где живет пользователь, и потом выводишь это, В окне, в html и в консоле. \
+
+const name_quest = 'Где вы проживаете?';
+
+document.write(name_quest);
+console.log(name_quest);
+alert(name_quest);
