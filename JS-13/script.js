7f9c71e36b98ec2fc8aa5a0ec6fb4513e94b3d1d
JoJovanna <75172430+JoJovanna@users.noreply.github.com>
2021-03-29 15:18:57 +0300

    first commit

--- JS-13/script.js
+++ JS-13/script.js
@@ -1,7 +1,14 @@
 //Напиши код, в котором ты спрашиваешь где живет пользователь, и потом выводишь это, В окне, в html и в консоле. \
 
-const name_quest = 'Где вы проживаете?';
+const result = prompt('В каком городе вы проживаете?');
 
-document.write(name_quest);
-console.log(name_quest);
-alert(name_quest);
+document.getElementById("name").innerHTML = result;
+console.log(result);
+
+//animation
+
+$('.Button').on('click', function() {
+    $('.Button').hide();
+    $('.name').hide();
+
+});
